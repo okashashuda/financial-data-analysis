012d4c34457b1d785abe30c0f7ce0d0b2dd6d345
scp10sfu <scp10@sfu.ca>
2024-11-11 18:02:53 -0800

    Did some preliminary graphs to get an idea of our data distribution.

--- clean_data.py
+++ clean_data.py
@@ -31,9 +31,58 @@ data = data.rename(columns={"What is your race / ethnicity? Select all that appl
 
 # keep only columns where 'annual expenses' or 'annual income' are NOT zero
 data = data[(data != 0).all(axis=1)]
-
+data = data.dropna()
 # convert int to float, for consistency
 data["Annual Income"] = data["Annual Income"].astype("float")
-
+#Orders both of the ordered categorical columns, to provide brevity in graphs.
+data['Age'] = pd.Categorical(data['Age'],categories=['<20','21-25','26-30','31-35','36-40','41-45','46-50','51-55','56-60','61-65','66-70'],ordered=True)
+data['Cost of Living'] = pd.Categorical(data['Cost of Living'],categories=['1-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100','101-110','111-120','121-130','131-140','141-150'],ordered=True)
+#Puts the cleaned data in a new csv.
 data.to_csv("data/clean_data.csv", index=True)
 
+
+# gender_counts = data['Gender'].value_counts()
+# plt.pie(gender_counts,labels=gender_counts.index)
+# plt.title("Gender Distribution")
+# plt.axis('equal')
+# plt.show()
+
+# sns.countplot(x='Education',data=data)
+# plt.title("Education Levels")
+# plt.show()
+
+
+sns.countplot(x='Age',data=data)
+plt.title("Age")
+plt.show()
+
+# gender_industry = pd.crosstab(data['Industry'],data['Gender'])
+# gender_industry.plot(kind='bar', stacked=True, colormap='Set2')
+# plt.title("Gender Distribution by Industry")
+# plt.ylabel('Count')
+# plt.show()
+
+# sns.countplot(x='Education', hue='Gender', data=data)
+# plt.title('Education by Gender')
+# plt.xticks(rotation=45)
+# plt.show()
+
+
+# g = sns.FacetGrid(data, col='Industry', col_wrap=4, height=3)
+# g.map(sns.histplot, 'Cost of Living')
+# g.set_axis_labels('Cost of Living', 'Count')
+# plt.show()
+
+
+def extract_midpoint(cost_range):
+    if(cost_range =='<20'):
+        return 20
+    lower, upper = map(int, cost_range.split('-'))
+    return (lower + upper) / 2
+
+data['Age Middle'] = data['Age'].apply(extract_midpoint)
+data['COL Middle'] = data['Cost of Living'].apply(extract_midpoint)
+corr = data[['Age Middle', 'COL Middle']].corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Correlation Heatmap')
+plt.show()
