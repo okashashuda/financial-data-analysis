7ca858f05455e2cd13e6e2e14d9ed9e9bd3a614f
Okasha Huda <okashahuda@hotmail.com>
2024-11-18 16:03:37 -0800

    attempting to do gaussianNB() machine learning model

--- mlmodels.py
+++ mlmodels.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.stats
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import FunctionTransformer
@@ -12,7 +13,36 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from skimage.color import rgb2lab
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_csv("data\clean_data.csv")
+data = pd.read_csv("cmpt353dataproject/data/clean_data.csv")
+
+categorical = data[["Ethnicity", "Gender", "Age", "Relationship Status", "Industry", "Education", "Cost of Living"]]
+numerical = data[["Annual Income", "Annual Expenses"]]
+
+
+label_encoders = {}
+for column in categorical.columns:
+    label_encoders[column] = LabelEncoder()
+    categorical[column] = label_encoders[column].fit_transform(categorical[column])
+
+scaler = StandardScaler()
+numerical_scaled = scaler.fit_transform(numerical)
+
+X = pd.concat([pd.DataFrame(numerical_scaled, columns=numerical.columns)])
+y = data["Relationship Status"].values
 
 def bayesian_model():
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    model.predict(X_valid, y_valid)
+    print(model.score(X_valid, y_valid))
+
+
+def main():
+    bayesian_model()
+
+    
